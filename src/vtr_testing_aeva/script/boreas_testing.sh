a62438cab39958bf4d7c800b835ffd21d69cefff
Yuchen Wu <cheneyuwu@gmail.com>
2022-04-17 00:08:58 -0400

    aeva tests

---
+++ src/vtr_testing_aeva/script/boreas_testing.sh
@@ -0,0 +1,44 @@
+echo "This script contains instructions to run all tests, do not run this script directly."
+exit 1
+
+## First launch RViz for visualization
+source /opt/ros/galactic/setup.bash               # source the ROS environment
+ros2 run rviz2 rviz2 -d ${VTRSRC}/rviz/lidar.rviz # launch rviz
+
+## Then in another terminal, launch rqt_reconfigure for control
+## current supported dynamic reconfigure parameters: control_test.play and controL_test.delay_millisec
+source /opt/ros/galactic/setup.bash
+ros2 run rqt_reconfigure rqt_reconfigure
+
+############################################################
+#### Now start another terminal and run testing scripts ####
+
+## Terminal Setup (Run Following Once)
+
+# Define the following environment variables VTRR=VTR RaDAR
+export VTRRROOT=${VTRROOT}/vtr_testing_radar # location of this repository CHANGE THIS!
+export VTRRDATA=${VTRDATA}/boreas/sequences  # dataset location (where the boreas-xxxxx folders at) CHANGE THIS!
+export VTRRRESULT=${VTRTEMP}/lidar/aeva      # result location MAYBE CHANGE THIS!
+mkdir -p ${VTRRRESULT}
+
+# Source the VTR environment with the testing package
+source ${VTRRROOT}/install/setup.bash
+
+# Choose a Teach (ODO_INPUT) and Repeat (LOC_INPUT) run from boreas dataset
+ODO_INPUT=boreas-2022-04-05-23-35
+LOC_INPUT=
+
+## Using ONE of the following commands to launch a test
+
+# (TEST 1) Perform data preprocessing on a sequence (e.g. keypoint extraction)
+bash ${VTRRROOT}/src/vtr_testing_aeva/script/test_preprocessing.sh ${ODO_INPUT}
+
+# (TEST 2) Perform odometry on a sequence
+bash ${VTRRROOT}/src/vtr_testing_aeva/script/test_odometry.sh ${ODO_INPUT}
+# Evaluation:
+bash ${VTRRROOT}/src/vtr_testing_aeva/script/test_odometry_eval.sh ${ODO_INPUT}
+
+# (TEST 3) Perform localization on a sequence (only run this after TEST 2)
+bash ${VTRRROOT}/src/vtr_testing_aeva/script/test_localization.sh ${ODO_INPUT} ${LOC_INPUT}
+# Evaluation:
+bash ${VTRRROOT}/src/vtr_testing_aeva/script/test_localization_eval.sh ${ODO_INPUT}
---
+++ src/vtr_testing_aeva/script/test_preprocessing.sh
@@ -0,0 +1,19 @@
+## this script assumes the following environment variables are set:
+##   VTRRROOT VTRRDATA VTRRRESULT
+## example usage: test_preprocessing.sh boreas-2021-09-02-11-42
+
+# Get arguments
+ODO_INPUT=$1
+
+# Log
+echo "Running preprocessing on sequence ${ODO_INPUT}, storing result to ${VTRRRESULT}/${ODO_INPUT}/${ODO_INPUT}"
+
+# Source the VTR environment with the testing package
+source ${VTRRROOT}/install/setup.bash
+
+ros2 run vtr_testing_aeva vtr_testing_aeva_aeva_preprocessing \
+  --ros-args -p use_sim_time:=true \
+  -r __ns:=/vtr \
+  --params-file ${VTRRROOT}/src/vtr_testing_aeva/config/aeva.yaml \
+  -p data_dir:=${VTRRRESULT}/${ODO_INPUT}/${ODO_INPUT} \
+  -p odo_dir:=${VTRRDATA}/${ODO_INPUT}
